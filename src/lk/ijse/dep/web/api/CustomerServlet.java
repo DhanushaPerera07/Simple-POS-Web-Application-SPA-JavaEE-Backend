582b3c49b927ef46eec2f89bfe539e4006ddba30
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2020-12-24 22:25:18 +0530

    Config CustomerServelt doGet method with the http status codes

--- src/lk/ijse/dep/web/api/CustomerServlet.java
+++ src/lk/ijse/dep/web/api/CustomerServlet.java
@@ -13,7 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,21 +26,33 @@ import java.util.List;
  **/
 @WebServlet(name = "CustomerServlet", urlPatterns = "/customers")
 public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Access-Control-Allow-Origin", CommonConstants.FRONTEND_URL);
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    }//doOptions
+
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String customerID = req.getParameter("id");
+
+        /* CORS policy */
+        resp.setHeader("Access-Control-Allow-Origin", CommonConstants.FRONTEND_URL);
 
         /* Let's get the connection pool using the created key value pair */
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
-
-        resp.setHeader("Access-Control-Allow-Origin", CommonConstants.FRONTEND_URL);
         resp.setContentType("application/json");
-        try (PrintWriter out = resp.getWriter()) {
 
-            try {
-                Class.forName(CommonConstants.MYSQL_DRIVER_CLASS_NAME);
-                Connection connection = cp.getConnection();
+        try {
+            PrintWriter out = resp.getWriter();
+            Class.forName(CommonConstants.MYSQL_DRIVER_CLASS_NAME);
 
-                Statement stm = connection.createStatement();
-                ResultSet rst = stm.executeQuery("SELECT * FROM customer");
+            try (Connection connection = cp.getConnection()) {
+                PreparedStatement pstm = connection.prepareStatement("SELECT * FROM customer" + ((customerID != null) ? " WHERE id=?" : ""));
+                if (customerID != null) {
+                    pstm.setObject(1, customerID);
+                }
+                ResultSet rst = pstm.executeQuery();
 
                 List<Customer> customerList = new ArrayList<Customer>();
 
@@ -52,27 +67,25 @@ public class CustomerServlet extends HttpServlet {
                     customerList.add(new Customer(Integer.toString(id), name, address, email, contact));
                 }
 
-                // Create Jsonb and serialize
-                Jsonb jsonb = JsonbBuilder.create();
-
-                out.println(jsonb.toJson(customerList));
-                connection.close();
-
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
+                if (customerID != null && customerList.isEmpty()) {
+//                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                } else {
+                    // Create Jsonb and serialize
+                    Jsonb jsonb = JsonbBuilder.create();
+                    out.println(jsonb.toJson(customerList));
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             }
-
-
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
 
 
     }//doGet
-
-    @Override
-    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setHeader("Access-Control-Allow-Origin", CommonConstants.FRONTEND_URL);
-        resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -122,4 +135,63 @@ public class CustomerServlet extends HttpServlet {
         }
 
     }//doPost
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
+
+        try {
+            Class.forName(CommonConstants.MYSQL_DRIVER_CLASS_NAME);
+            Connection connection = cp.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("DELETE FROM customer WHERE `id` = ?");
+
+            pstm.setObject(1, id);
+            boolean success = pstm.executeUpdate() > 0;
+            if (success) {
+                /* deleted successfully ---> 204 status code */
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+
+            connection.close();
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }//doDelete
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        Jsonb jsonb = JsonbBuilder.create();
+        Customer customer = jsonb.fromJson(req.getReader(), Customer.class);
+
+        BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
+        try {
+            Class.forName(CommonConstants.MYSQL_DRIVER_CLASS_NAME);
+            Connection connection = cp.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("UPDATE customer SET name=?,address=?, email=?,contact=? WHERE id=?");
+            pstm.setObject(1, customer.getName());
+            pstm.setObject(2, customer.getAddress());
+            pstm.setObject(3, customer.getEmail());
+            pstm.setObject(4, customer.getContact());
+            pstm.setObject(5, id);
+
+            boolean success = pstm.executeUpdate() > 0;
+
+            if (success) {
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    } //doPut
 }
